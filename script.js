239bddf70d0a8fee162ffdff641ced6848afd2a0
jadenryank <jadenryanknguyen@gmail.com>
2025-02-08 21:55:29 +0000

    main finish

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ import {CARD_IMAGE, play_game} from "./js/game.js"; // We are 'importing' variab
 // === START #1.1 ===
 // TODO #1.1: Add your with your name!
 // Remove the '/*' and '*/ once you do
-/*
-const PLAYER_NAME ? ???;
-document.getElementById('???').textContent = ???;
+
+const PLAYER_NAME = "Rook";
+document.getElementById('p1_name').textContent = PLAYER_NAME;
 console.log("Let's play " + PLAYER_NAME +"!");
-*/
+
 // === END #1.1 ===
 
 /**
@@ -34,24 +34,38 @@ export function draw_and_update_card(deck, player_score) {
     // === START #2 ===
     // TODO #2: Complete the if-else tree!
     // Remove the '/*' and '*/ once you do
-    /*
+    
     if(value_str == "ace"){
         player_score = player_score + 1;
     } else if(value_str == "two"){
         player_score = player_score + 2;
-    } ...
-     */
+    }   else if (value_str == "three") {
+        player_score = player_score + 3;
+    } else if (value_str == "four") {
+        player_score = player_score + 4;
+    } else if (value_str == "five") {
+        player_score = player_score + 5;
+    } else if (value_str == "six") {
+        player_score = player_score + 6;
+    } else if (value_str == "seven") {
+        player_score = player_score + 7;
+    } else if (value_str == "eight") {
+        player_score = player_score + 8;
+    } else if (value_str == "nine") {
+        player_score = player_score + 9;
+    } else {
+        // Drew a ten or face card and all faces are equal to 10
+        player_score = player_score + 10;
+    }
+
     // === END #2 ===
 
     // === START #3 ===
     // TODO #3: Did the player lose?
     // REMEMBER: The player's score is stored in the 'player_score' variable
     // Remove the '/*' and '*/ to once you do
-    /*
-    __(player_score ? 21){
+    __(player_score ? 21)
         alert("You went over 21!");
-    }
-     */
     // === END #3 ===
 
     return player_score;
